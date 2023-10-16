400ba59ad9c1b7ac9ee6baa6bcbdfb3c363df4cb
Noumannomi123 <42030674+Noumannomi123@users.noreply.github.com>
2023-10-16 15:00:53 +0500

    Revert "Add files via upload"

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,14 +1,10 @@
 package login;
-import mainpage.Home;
-import database.Database;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
-import utils.JTextFieldLimit;
 import utils.MyIcon;
 
 import javax.swing.JTextField;
@@ -19,8 +15,6 @@ import java.awt.Color;
 import javax.swing.border.BevelBorder;
 import javax.swing.JPasswordField;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
@@ -28,13 +22,12 @@ import java.awt.Cursor;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
-
 public class Login extends JFrame {
 
     private JPanel contentPane;
     private JLabel txtGymManagementSystem;
-    private JTextField username_1;
-    private JPasswordField password_1;
+    private JTextField textField;
+    private JPasswordField textField_1;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -51,7 +44,7 @@ public class Login extends JFrame {
 
     public Login() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 396, 487);
+        setBounds(100, 100, 400, 430);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
@@ -60,16 +53,16 @@ public class Login extends JFrame {
         JPanel panel = new JPanel();
         panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
         panel.setBackground(new Color(0, 0, 255));
-        panel.setBounds(0, 0, 406, 69);
+        panel.setBounds(0, 0, 386, 69);
         contentPane.add(panel);
         panel.setLayout(null);
 
         txtGymManagementSystem = new JLabel();
         txtGymManagementSystem.setForeground(new Color(255, 255, 255));
         txtGymManagementSystem.setHorizontalAlignment(SwingConstants.CENTER);
-        txtGymManagementSystem.setFont(new Font("Monospaced", Font.BOLD, 40));
+        txtGymManagementSystem.setFont(new Font("Monospaced", Font.BOLD, 30));
         txtGymManagementSystem.setText("GymPro Vision");
-        txtGymManagementSystem.setBounds(10, 0, 366, 69);
+        txtGymManagementSystem.setBounds(10, 0, 366, 80);
         panel.add(txtGymManagementSystem);
 
         
@@ -82,6 +75,8 @@ public class Login extends JFrame {
         panel_1.setLayout(null);
 
         
+
+
         JLabel lblNewLabel = new JLabel("Login");
         lblNewLabel.setForeground(new Color(255, 255, 255));
         lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 24));
@@ -90,7 +85,7 @@ public class Login extends JFrame {
 
         JPanel panel_2 = new JPanel();
         panel_2.setBackground(new Color(0,0,0,0));
-        panel_2.setBounds(51, 208, 300, 147);
+        panel_2.setBounds(51, 166, 300, 130);
         contentPane.add(panel_2);
         panel_2.setLayout(null);
 
@@ -104,22 +99,19 @@ public class Login extends JFrame {
         lblNewLabel_2.setBounds(10, 82, 96, 14);
         panel_2.add(lblNewLabel_2);
 
-        username_1 = new JTextField();
-        username_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-        username_1.setBounds(116, 35, 119, 20);
-        panel_2.add(username_1);
-        username_1.setColumns(10);
-        username_1.setDocument(new JTextFieldLimit(10)); // Limit User Name to 10 characters
-        int borderWidth = 2;
-        username_1.setBorder(new LineBorder(Color.GRAY, borderWidth, true)); // true for rounded border
-        
-        password_1 = new JPasswordField();
-        password_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-        password_1.setBounds(116, 82, 119, 20);
-        panel_2.add(password_1);
-        password_1.setColumns(10);
-        password_1.setDocument(new JTextFieldLimit(8)); // Limit Password to 8 characters
-        password_1.setBorder(new LineBorder(Color.GRAY, borderWidth, true)); // true for rounded border
+        textField = new JTextField();
+        textField.setFont(new Font("SansSerif", Font.PLAIN, 14));
+        textField.setBounds(116, 35, 119, 20);
+        panel_2.add(textField);
+        textField.setColumns(10);
+        textField.setDocument(new JTextFieldLimit(10)); // Limit User Name to 10 characters
+
+        textField_1 = new JPasswordField();
+        textField_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
+        textField_1.setBounds(116, 82, 119, 20);
+        panel_2.add(textField_1);
+        textField_1.setColumns(10);
+        textField_1.setDocument(new JTextFieldLimit(8)); // Limit Password to 8 characters
 
         char bulletChar = '\u2022';
 
@@ -127,9 +119,9 @@ public class Login extends JFrame {
         chckbxNewCheckBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (chckbxNewCheckBox.isSelected()) {
-                    password_1.setEchoChar((char) 0); // Display the password as plain text
+                    textField_1.setEchoChar((char) 0); // Display the password as plain text
                 } else {
-                    password_1.setEchoChar(bulletChar); // Hide the password
+                    textField_1.setEchoChar(bulletChar); // Hide the password
                 }
             }
         });
@@ -138,9 +130,8 @@ public class Login extends JFrame {
         panel_2.add(chckbxNewCheckBox);
 
         JPanel panel_3 = new JPanel();
-        panel_3.setBorder(new EmptyBorder(0, 0, 0, 0));
         panel_3.setBackground(new Color(0, 0, 0, 0)); // Transparent background
-        panel_3.setBounds(96, 355, 179, 69);
+        panel_3.setBounds(96, 307, 179, 65);
         contentPane.add(panel_3);
         panel_3.setLayout(null);
 
@@ -154,63 +145,35 @@ public class Login extends JFrame {
         panel_3.add(btnNewButton);
         setResizable(false);
 
-        JPanel panel_5 = new JPanel();
-        panel_5.setBounds(88, 165, 213, 62);
-        panel_5.setBackground(new Color(0,0,0,0));
-        contentPane.add(panel_5);
-   
-        // Create a label for the combo box
-        JLabel lblNewLabel_3 = new JLabel("User Type:");
-        lblNewLabel_3.setToolTipText("Select the user type to login.");
-        lblNewLabel_3.setFont(new Font("SansSerif", Font.BOLD, 16));
-        lblNewLabel_3.setBounds(10, 11, 96, 20);
-        lblNewLabel_3.setIcon(new ImageIcon("D:\\JavaDocuments\\GymSystem\\pictures\\UserType.png"));
-        panel_5.add(lblNewLabel_3);
         
-        // Create a combo box with user types
-        String[] userTypes = {"Member", "Staff", "Admin", "Registration Manager"};
-        JComboBox<String> usertype = new JComboBox<>(userTypes);
-        usertype.setFont(new Font("SansSerif", Font.PLAIN, 14));
-        usertype.setBounds(116, 11, 96, 20);
-        panel_5.add(usertype);
-        
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = textField.getText();
+                char[] password = textField_1.getPassword();
+
+                if (username.isEmpty()) {
+                    JOptionPane.showMessageDialog(contentPane, "Username cannot be empty", "Invalid Username", JOptionPane.ERROR_MESSAGE);
+                } else if (password.length != 8) {
+                    JOptionPane.showMessageDialog(contentPane, "Password must be 8 characters long", "Invalid Password", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    // Perform login action
+                    // Replace this with your login logic
+                }
+            }
+        });
+
         JPanel panel_4 = new JPanel();
         panel_4.setBorder(new EmptyBorder(0, 0, 0, 0));
         panel_4.setBackground(new Color(240, 240, 240)); 
-        panel_4.setBounds(0, 67, 396, 458);
+        panel_4.setBounds(0, 67, 386, 326);
         contentPane.add(panel_4);
         panel_4.setLayout(null);
         
         JLabel backgroundLabel = new JLabel();
         backgroundLabel.setIcon(new MyIcon(new ImageIcon("D:\\\\JavaDocuments\\\\GymSystem\\\\pictures\\\\login.jpeg").getImage(),400,400));
         
-        backgroundLabel.setBounds(-12, -14, 420, 430);
+        backgroundLabel.setBounds(-14, -14, 400, 340);
         panel_4.add(backgroundLabel);
-        
-        btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String username = username_1.getText();
-                char[] password = password_1.getPassword();
-                String uType = (String) usertype.getSelectedItem();
-                if (username.isEmpty()) {
-                    JOptionPane.showMessageDialog(contentPane, "Username cannot be empty", "Invalid Username", JOptionPane.ERROR_MESSAGE);
-                } else if (password.length != 8) {
-                    JOptionPane.showMessageDialog(contentPane, "Password must be 8 characters long", "Invalid Password", JOptionPane.ERROR_MESSAGE);
-                } else {
-                	Database db = new Database();
-                    if (db.verifyLogin(username, uType, password)) {
-                        // Successful login, close current JFrame
-                        dispose();
-                        Home newFrame = new Home();
-                        newFrame.setVisible(true);
-                        
-                    } else {
-                        // Show error dialog for incorrect username or password
-                        JOptionPane.showMessageDialog(contentPane, "Incorrect username or password. Please try again.", "Login Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                	
-                }
-            }
-        });
+        //assdsd
     }
 }
